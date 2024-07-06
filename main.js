7ed14374983f1f849595313708dd516b2f2d666b
Radiumrishu <141204783+Radiumrishu@users.noreply.github.com>
2024-07-06 18:24:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Web Developer", "Graphic designer", "CATIA designer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
